5bab1760412d806dff1378f1a65275f9ec172f0e
Abhimanyu Singh <abhimanyus0726@gmail.com>
2025-10-02 18:13:26 +0530

    first commit

--- src/graphs/graph_builder.py
+++ src/graphs/graph_builder.py
@@ -1,23 +1,24 @@
-from langgraph.graph import StateGraph,START,END
+from langgraph.graph import StateGraph, START, END
 from src.llms.groqllm import GroqLLM
 from src.states.blogstate import BlogState
 from src.nodes.blog_node import BlogNode
 
 class GraphBuilder:
     def __init__(self,llm):
-        self.llm = llm
-        self.graph = StateGraph(BlogState)
-    
+        self.llm=llm
+        self.graph=StateGraph(BlogState)
+
     def build_topic_graph(self):
         """
-        Build a graph to generate blogs based on topic
+        Build a graph to generate blogss based on topic
         """
-        self.blog_node = BlogNode(self.llm)
-        # nodes
-        self.graph.add_node("title_creation",self.blog_node.title_creation)
-        self.graph.add_node("content_generation",self.blog_node.content_generation)
+        self.blog_node_obj=BlogNode(self.llm)
+        print(self.llm)
+        ## Nodes
+        self.graph.add_node("title_creation", self.blog_node_obj.title_creation)
+        self.graph.add_node("content_generation",self.blog_node_obj.content_generation)
 
-        # edges
+        ## Edges
         self.graph.add_edge(START,"title_creation")
         self.graph.add_edge("title_creation","content_generation")
         self.graph.add_edge("content_generation",END)
@@ -28,21 +29,24 @@ class GraphBuilder:
         """
         Build a graph for blog generation with inputs topic and language
         """
-        self.blog_node = BlogNode(self.llm)
-        # nodes
-        self.graph.add_node("title_creation",self.blog_node.title_creation)
-        self.graph.add_node("content_generation",self.blog_node.content_generation)
-        self.graph.add_node("hindi_translation",lambda state: self.blog_node.translation({**state, "current_language":"hindi"}))
-        self.graph.add_node("french_translation",lambda state: self.blog_node.translation({**state, "current_language":"french"}))
-        self.graph.add_node("route",self.blog_node.route)
+        self.blog_node_obj=BlogNode(self.llm)
+        print(self.llm)
+        ## Nodes
+        self.graph.add_node("title_creation", self.blog_node_obj.title_creation)
+        self.graph.add_node("content_generation",self.blog_node_obj.content_generation)
+        self.graph.add_node("hindi_translation",lambda state: self.blog_node_obj.translation({**state, "current_language": "hindi"}))
+        self.graph.add_node("french_translation",lambda state: self.blog_node_obj.translation({**state, "current_language": "french"}))
+        self.graph.add_node("route",self.blog_node_obj.route)
 
-        # edge
-        self.graph.add_edge(START,"title_creation")
-        self.graph.add_edge("title_creation","content_generation")
-        self.graph.add_edge("content_generation","route")
+        ## edges and conditional edges
+        self.graph.add_edge(START, "title_creation")
+        self.graph.add_edge("title_creation", "content_generation")
+        self.graph.add_edge("content_generation", "route")
+
+        ## conditional edge
         self.graph.add_conditional_edges(
             "route",
-            self.blog_node.route_decision,
+            self.blog_node_obj.route_decision,
             {
                 "hindi":"hindi_translation",
                 "french":"french_translation"
@@ -50,12 +54,14 @@ class GraphBuilder:
         )
         self.graph.add_edge("hindi_translation",END)
         self.graph.add_edge("french_translation",END)
+        return self.graph
 
+    
     def setup_graph(self,usecase):
-        if usecase == "topic":
+        if usecase=="topic":
             self.build_topic_graph()
-        
-        if usecase == "language":
+        if usecase=="language":
+            print("Language block")
             self.build_language_graph()
         return self.graph.compile()
-
+    
